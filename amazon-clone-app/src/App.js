5b211057362e85c6b9d9a7771861bafdb614e524
ZakwanAbid <mzakwanabid15@gmail.com>
2022-06-28 00:23:18 +0500

    Second commit

--- amazon-clone-app/src/App.js
+++ amazon-clone-app/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import {Header, Home} from './Components/index';
+import { BrowserRouter,Route,Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes> 
+          <Route path="/" element={
+            <>
+            <Header />
+            <Home />
+            </>
+          } />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
